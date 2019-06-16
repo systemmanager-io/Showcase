d540b54b6b81ffaf65d15b0df782f9aad9fddded
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-06-16 20:44:19 +0200

    Fixed the footer errors regarding the columns

--- resources/js/components/TopBar.js
+++ resources/js/components/TopBar.js
@@ -10,7 +10,7 @@ export default class SystemManagerShowcaseTopBar extends React.Component {
         return (
             <div>
                 <div className="center-content showcase showcase-padding">
-                    <h1 className="align-content-center">SystemManager</h1>
+                    <h1 className="align-content-center">SystemManager Monitoring Software</h1>
                     <h5 className="align-content-center">The best solution to manage your VPSes and Dedicated Boxes</h5>
                     <Link hidden={location.pathname !== "/" ? true : false} to="/demo"><Button className="top-buttons">Demo</Button></Link>
                     <Link hidden={location.pathname !== "/" ? true : false} to="/getstarted"><Button className="top-buttons">Get started</Button></Link>
