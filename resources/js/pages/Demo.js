0c72b9702b71cb997ebb884ef8c7c2e518ad1f89
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-06-19 12:59:20 +0200

    Update resources/js/pages/Demo.js

--- resources/js/pages/Demo.js
+++ resources/js/pages/Demo.js
@@ -1,11 +1,23 @@
 import * as React from 'react'
-import {Button} from "reactstrap";
+import {Button, Card} from "reactstrap";
 
 export default class Demo extends React.Component {
     render() {
         return(
             <div  className="container center-content showcase-padding">
                 <h2>Not convinced yet? Use our demo to see what it can!</h2>
+                <hr />
+                <div className={col-3}>
+                    <Card>
+                        <CardBody>
+                            <h5>Demo Login</h5>
+
+
+                            <Button>Enter the demo!</Button>
+
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
         )
     }
