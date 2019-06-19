661ce17a8875ef0bda113e4b9b5aa3014259af0d
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-06-19 13:22:13 +0200

    Update Demo.js

--- resources/js/pages/Demo.js
+++ resources/js/pages/Demo.js
@@ -4,7 +4,7 @@ import {Button, Card, CardBody} from "reactstrap";
 export default class Demo extends React.Component {
     render() {
         return(
-            <div  className="container center-content showcase-padding">
+            <div className="container center-content showcase-padding">
                 <h2>Not convinced yet? Use our demo to see what it can!</h2>
                 <hr />
                 <div className="row">
