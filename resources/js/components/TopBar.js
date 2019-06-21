f4b8875a1dd67d0b51086ce35fab778a65c79967
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-06-21 13:49:50 +0200

    Changed the a few buttons to the correct color and things, made the Alert bar for staging somewhat more visible

--- resources/js/components/TopBar.js
+++ resources/js/components/TopBar.js
@@ -10,8 +10,8 @@ export default class SystemManagerShowcaseTopBar extends React.Component {
                 <div className="center-content showcase showcase-padding">
                     <h1 className="align-content-center">SystemManager Monitoring Software</h1>
                     <h5 className="align-content-center">The all in one server management system!</h5>
-                    <Link hidden={location.pathname !== "/" ? true : false} to="/demo"><Button className="top-buttons">Demo</Button></Link>
-                    <Link hidden={location.pathname !== "/" ? true : false} to="/getstarted"><Button className="top-buttons">Get started</Button></Link>
+                    <Link hidden={location.pathname !== "/" ? true : false} to="/demo" className="btn top-buttons">Demo</Link>
+                    <Link hidden={location.pathname !== "/" ? true : false} to="/getstarted" className="btn top-buttons">Get started</Link>
                 </div>
             </div>
         )
