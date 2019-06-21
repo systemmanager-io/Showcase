e4b1ceca322ba5c909f1114dbc130952fdc564fc
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-06-21 12:32:35 +0200

    Fixed broken meta tag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token"
+        <meta name="csrf-token"/>
         <meta name="theme-color" content="#8c0032">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/showcase.css') }}">
