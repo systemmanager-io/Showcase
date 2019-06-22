d14e5f58d10fb3474a13cba934faa7d4df2a6aee
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-06-23 00:54:23 +0200

    Added text to the highlighted features

--- resources/js/components/HighlightedFeatures.js
+++ resources/js/components/HighlightedFeatures.js
@@ -1,4 +1,6 @@
 import * as React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLock} from "@fortawesome/free-solid-svg-icons";
 
 export default class HighlightedFeatures extends React.Component {
 
@@ -8,20 +10,25 @@ export default class HighlightedFeatures extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-sm-12 col-md-6 col-lg-4">
-                        Security
+                        <h3>Security</h3>
+                        <hr />
+                        <p>Security is the most important thing for SystemManager! We use the latest technology to secure your data! Every problem found in the security of SystemManager will be dealt with in shot order!</p>
                         <br/>
                     </div>
                     <div className="col-12 col-sm-12 col-md-6 col-lg-4">
-                        User Friendly
-                        <br/>
+                        <h3>User Friendly</h3>
+                        <hr/>
+                        <p>No difficult interfaces! We crafted one beautiful panel to manage every server with! Do you miss something? Contact us! We will look if we are going to include it!</p>
                     </div>
                     <div className="col-12 col-sm-12 col-md-6 col-lg-4">
-                            Native Apps
-                        <br/>
+                        <h3>Native Apps</h3>
+                        <hr/>
+                        <p>As part of our software line up apps will follow! For android and iOS! This way you won't have to use a third-party app! Every change in our app has been curated by us!</p>
                     </div>
                     <div className="col-12 col-sm-12 col-md-6 col-lg-4">
-                        Free and Opensource
-                        <br/>
+                        <h3>Free and Opensource</h3>
+                        <hr/>
+                        <p>Hate to pay for software? We do too! You can get SystemManager COMPLETELY for free! The only YOU have to PAY for is custom features! Or one of our paid packages! We want to clarify. ONCE a feature has been released to the CORE, it will NEVER turn into a paid feature!</p>
                     </div>
                     <div className="col-12 col-sm-12 col-md-6 col-lg-4">
                         <br/>
