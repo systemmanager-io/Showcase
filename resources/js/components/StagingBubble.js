dcc4f5fc0dec2fab2b5604b7a09ba74afdd8fb80
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-06-26 16:40:42 +0200

    Update StagingBubble.js

--- resources/js/components/StagingBubble.js
+++ resources/js/components/StagingBubble.js
@@ -11,7 +11,6 @@ export default class DevelopmentBubble extends React.Component {
             <hr />
             <div className="center-content">
                 <a><FontAwesomeIcon icon={faExclamationTriangle}/> You are viewing the staging area <FontAwesomeIcon icon={faExclamationTriangle}/></a><br />
-                <a style={{fontSize: "0.75rem"}}>You can find stuff here you will not see on systemmanager.io</a>
                 <br/>
                 <a className="btn systemManagerButton" href="https://systemmanager.io">Looking for systemmanager.io?</a>
 
