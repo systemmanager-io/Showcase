766570c9b6aa8b48de8bc7362d41fc5178080b29
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-06-27 09:40:27 +0200

    Update ProductionBubble.js

--- resources/js/components/ProductionBubble.js
+++ resources/js/components/ProductionBubble.js
@@ -11,10 +11,10 @@ export default class DevelopmentBubble extends React.Component {
             <div className="center-content">
                 <br/>
                 <FontAwesomeIcon className="productWip col-3" icon={faExclamationTriangle}/>
-                <div className="col-9">
+                <div className="row"><div className="col-9">
                 <a>This is an unreleased product!</a><br/>
                 <a>Things are subjected to change!</a>
-                </div>
+                </div></div>
             </div>
         )
     }
