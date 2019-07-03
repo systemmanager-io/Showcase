50a4cf22e9487eec271184c8448e181940c870b7
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-07-03 22:50:03 +0200

    Changes all around!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,42 +6,45 @@ export default class SystemManagerShowcaseNavbar extends React.Component {
 
     state = {
         collapsed: true
-    }
+    };
 
     toggleNavbar() {
         this.setState({collapsed: !this.state.collapsed})
     }
 
+
+
     render() {
         return (
-            <nav className="flex items-center justify-between flex-wrap navbar-color p-3">
-                <div className="flex items-center flex-shrink-0 text-white mr-6">
-                    <Link title="SystemManager" to="/"
-                          className="navbar-brand-hover navbar-brand">SystemManager</Link>
-                </div>
-                <div className="block lg:hidden">
-                    <button
-                        className="flex items-center text-white hover:text-white hover:border-white">
-                        <svg className="fill-current h-6 w-7" viewBox="0 0 20 20"
-                             xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
-                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
-                        </svg>
-                    </button>
-                </div>
-                <div className="w-full lg:flex lg:items-center lg:w-auto hidden sm:hidden">
-                    <Link title="Homepage   "
-                          className="block mt-4 lg:inline-block lg:mt-0 text-white primary-light-text mr-4"
-                          to="/">Home</Link>
-                    <Link title="Demos of SystemManager"
-                          className="block mt-4 lg:inline-block lg:mt-0 text-white primary-light-text mr-4"
-                          to="/demo">Demo</Link>
-                    <Link title="Documentation of SystemManager"
-                          className="block mt-4 lg:inline-block lg:mt-0 text-white primary-light-text mr-4"
-                          to="/documentation">Documentation</Link>
-                    <Link title="Download SystemManager"
-                          className="inline-block px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent primary-light-text hover:bg-white mt-4 lg:mt-0"
-                          to="/documentation/getstarted">Get Started</Link>
-                </div>
+            <nav className="flex items-center justify-between flex-wrap navbar-color p-4 lg:p-3">
+                    <div className="flex items-center flex-shrink-0 text-white mr-6">
+                        <Link title="SystemManager" to="/"
+                              className="navbar-brand-hover navbar-brand">SystemManager</Link>
+                    </div>
+                    <div className="block lg:hidden">
+                        <button
+                            onClick={this.toggleNavbar.bind(this)}
+                            className="flex items-center text-white hover:text-white hover:border-white">
+                            <svg className="fill-current h-6 w-7" viewBox="0 0 20 20"
+                                 xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
+                                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <div className={this.state.collapsed ? "w-full lg:flex lg:items-center lg:w-auto hidden" : "w-full lg:flex lg:items-center navbar-animation lg:w-auto"}>
+                        <Link title="Homepage"
+                              className="block mt-4 lg:inline-block lg:mt-0 text-white primary-light-text mr-4"
+                              to="/">Home</Link>
+                        <Link title="Demos of SystemManager"
+                              className="block mt-4 lg:inline-block lg:mt-0 text-white primary-light-text mr-4"
+                              to="/demo">Demo</Link>
+                        <Link title="Documentation of SystemManager"
+                              className="block mt-4 lg:inline-block lg:mt-0 text-white primary-light-text mr-4"
+                              to="/documentation">Documentation</Link>
+                        <Link title="Download SystemManager"
+                              className="inline-block px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent primary-light-text hover:bg-white mt-4 lg:mt-0"
+                              to="/documentation/getstarted">Get Started</Link>
+                    </div>
             </nav>
         )
     }
--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import {Card, CardBody} from "reactstrap";
 import {Fragment} from "react";
 import SystemManagerShowcaseTopBar from "../components/TopBar";
 
@@ -13,65 +12,59 @@ export default class Demo extends React.Component {
                     <h2>Not convinced yet? Use our demo to see what it can!</h2>
                     <h5>This demo has certain features disabled and stuff hidden</h5>
                     <hr/>
-                    <div className="flew flex">
+                    <div className="flex flex-wrap">
                         <div className="w-full md:w-6/12 lg:w-4/12">
-                            <Card>
-                                <CardBody>
-                                    <h3>Demo Status Page</h3>
-                                    <hr/>
-
-
-                                    <a href="https://status.demo.systemmanager.io"
-                                       title="See the demo status page"
-                                       className="btn systemManagerButton systemManagerButton-border">Look at the
-                                        status page</a>
-
-                                </CardBody>
-                            </Card>
+                            <div className="bg-white rounded overflow-hidden shadow-lg lg:m-4">
+                                <div className="px-6 py-4">
+                                    <div className="text-black font-bold text-2xl mb-2">Demo Status Page</div>
+                                    <p className="text-gray-700 text-base p-4">
+                                        <a href="https://status.demo.systemmanager.io"
+                                           title="See the demo status page"
+                                           className="btn systemManagerButton systemManagerButton-border">Look at the
+                                            status page</a>
+                                    </p>
+                                </div>
+                            </div>
                             <br/>
                         </div>
                         <div className="w-full md:w-6/12 lg:w-4/12">
-                            <Card>
-                                <CardBody>
-                                    <h3>Demo User Login</h3>
-                                    <hr/>
-                                    The account credentials for user "user"
-                                    <br/>
-                                    <br/>
-                                    <div className="demoCredentials">
+                            <div className="bg-white rounded overflow-hidden shadow-lg lg:m-4">
+                                <div className="px-6 py-4">
+                                    <div className="text-black font-bold text-2xl mb-2">Demo User Login</div>
+                                    <div>The account credentials for user "user"</div>
+                                    <p className="text-gray-700 text-base p-4">
                                         User: user<br/>
-                                        Password: password<br/>
-                                    </div>
-                                    <br/>
-                                    <a href="https://demo.systemmanager.io/login"
-                                       title="See the demo panel as a user"
-                                       className="btn systemManagerButton systemManagerButton-border">Enter the demo
-                                        as a user</a>
+                                        Password: password
+                                        <br/>
+                                        <br/>
+                                        <a href="https://demo.systemmanager.io/login"
+                                           title="See the demo panel as a user"
+                                           className="btn systemManagerButton systemManagerButton-border">Enter the demo
+                                            as a user</a>
+                                    </p>
 
-                                </CardBody>
-                            </Card>
+                                </div>
+                            </div>
                             <br/>
                         </div>
                         <div className="w-full md:w-6/12 lg:w-4/12">
-                            <Card>
-                                <CardBody>
-                                    <h3>Demo Admin Login</h3>
-                                    <hr/>
-                                    The account credentials for user "admin"
-                                    <br/>
-                                    <br/>
-                                    <div className="demoCredentials">
+                            <div className="bg-white rounded overflow-hidden shadow-lg lg:m-4">
+                                <div className="px-6 py-4">
+                                    <div className="text-black font-bold text-2xl mb-2 p-3">Demo Admin Login</div>
+                                    <div>The account credentials for user "admin"</div>
+                                    <p className="text-gray-700 text-base p-4">
                                         User: admin<br/>
-                                        Password: password<br/>
-                                    </div>
-                                    <br/>
-                                    <a href="https://demo.systemmanager.io/login"
-                                       title="See the demo panel as a admin"
-                                       className="btn systemManagerButton systemManagerButton-border">Enter the demo
-                                        as an admin</a>
-
-                                </CardBody>
-                            </Card>
+                                        Password: password
+                                        <br/>
+                                        <br/>
+                                        <a href="https://demo.systemmanager.io/login"
+                                           title="See the demo panel as a admin"
+                                           className="btn systemManagerButton systemManagerButton-border">Enter the
+                                            demo
+                                            as an admin</a>
+                                    </p>
+                                </div>
+                            </div>
                             <br/>
                         </div>
                     </div>
