4021a832022f46308e10069b1cbc2410d62a93e2
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-07-03 22:01:39 +0200

    Made it more like a card then anything flat on a surface

--- src/components/HighlightedFeatures.js
+++ src/components/HighlightedFeatures.js
@@ -7,40 +7,75 @@ export default class HighlightedFeatures extends React.Component {
         return (
             <div className="flex flex-wrap">
                 <div className="w-full md:w-6/12 lg:w-4/12">
-                    <h3 className="text-2xl">Security</h3>
-                    <hr/>
-                    <p>Security is the most important thing for SystemManager! We use the latest technology to
-                        secure your data! Every problem found in the security of SystemManager will be dealt with in
-                        short order!</p>
+                    <div className="bg-white rounded overflow-hidden shadow-lg lg:m-4">
+                        <div className="px-6 py-4">
+                            <div className="text-black font-bold text-2xl mb-2">Security</div>
+                            <hr/>
+                            <p className="text-gray-700 text-base">Security is the most important thing for
+                                SystemManager! We use the latest technology to
+                                secure your data! Every problem found in the security of SystemManager will be dealt
+                                with in
+                                short order!</p>
+                            <br/>
+                        </div>
+                    </div>
                     <br/>
                 </div>
+
                 <div className="w-full md:w-6/12 lg:w-4/12">
-                    <h3 className="text-2xl">User Friendly</h3>
-                    <hr/>
-                    <p>No difficult interfaces! We crafted one beautiful panel to manage every server with! Do you
-                        miss something? Contact us! We will look if we are going to include it!</p>
+                    <div className="bg-white rounded overflow-hidden shadow-lg lg:m-4">
+                        <div className="px-6 py-4">
+                            <div className="text-black font-bold text-2xl mb-2">User Friendly</div>
+                            <hr/>
+                            <p className="text-gray-700 text-base">No difficult interfaces! We crafted one beautiful panel to manage every server with! Do
+                                you
+                                miss something? Contact us! We will look if we are going to include it!</p>
+                        </div>
+                    </div>
+                    <br/>
                 </div>
+
                 <div className="w-full md:w-6/12 lg:w-4/12">
-                    <h3 className="text-2xl">Native Apps</h3>
-                    <hr/>
-                    <p>As part of our software line up apps will follow! For Android and iOS! This way you won't
-                        have to use a third-party app! Every change in our app has been curated by us!</p>
+                    <div className="bg-white rounded overflow-hidden shadow-lg lg:m-4">
+                        <div className="px-6 py-4">
+                            <div className="text-black font-bold text-2xl mb-2">Native Apps</div>
+                            <hr/>
+                            <p className="text-gray-700 text-base">As part of our software line up apps will follow! For Android and iOS! This way you won't
+                                have to use a third-party app! Every change in our app has been curated by us!</p>
+                        </div>
+                    </div>
+                    <br/>
                 </div>
+
                 <div className="w-full md:w-6/12 lg:w-4/12">
-                    <h3 className="text-2xl">Free and Opensource</h3>
-                    <hr/>
-                    <p>SystemManager is 100% free and licensed under a MIT license. The code of our products is
-                        opensource as well. <br/><br/> Even this website is opensource! You can find it on github!
-                        {/*<br/><br/>When a feature comes to the core package it will never be a paid feature. That is a promise.*/}
-                    </p>
+                    <div className="bg-white rounded overflow-hidden shadow-lg lg:m-4">
+                        <div className="px-6 py-4">
+                            <div className="text-black font-bold text-2xl mb-2">Free and Opensource</div>
+                            <hr/>
+                            <p className="text-gray-700 text-base">SystemManager is 100% free and licensed under a MIT license. The code of our products is
+                                opensource as well. <br/><br/> Even this website is opensource! You can find it on
+                                github!
+                                {/*<br/><br/>When a feature comes to the core package it will never be a paid feature. That is a promise.*/}
+                            </p>
+                        </div>
+                    </div>
+                    <br/>
                 </div>
+
+
                 <div className="w-full md:w-6/12 lg:w-4/12">
-                    <h3 className="text-2xl">Lightweight</h3>
-                    <hr/>
-                    <p>SystemManager and all its components are build to be fast and lightweight on your systems
-                        {/*<br/><br/>When a feature comes to the core package it will never be a paid feature. That is a promise.*/}
-                    </p>
+                    <div className="bg-white rounded overflow-hidden shadow-lg lg:m-4">
+                        <div className="px-6 py-4">
+                            <div className="text-black font-bold text-2xl mb-2">Lightweight</div>
+                            <hr/>
+                            <p className="text-gray-700 text-base">SystemManager and all its components are build to be fast and lightweight on your systems
+                                {/*<br/><br/>When a feature comes to the core package it will never be a paid feature. That is a promise.*/}
+                            </p>
+                        </div>
+                    </div>
+                    <br/>
                 </div>
+
                 <div className="w-full md:w-6/12 lg:w-4/12">
                     <br/>
                 </div>
