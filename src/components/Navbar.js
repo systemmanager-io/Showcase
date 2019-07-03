a49bc292728f6a34274006b7e402405232ce031f
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-07-03 17:42:42 +0200

    More changes to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,18 +17,18 @@ export default class SystemManagerShowcaseNavbar extends React.Component {
             <nav className="flex items-center justify-between flex-wrap navbar-color p-4">
                 <div className="flex items-center flex-shrink-0 text-white mr-6">
                     <Link title="SystemManager" to="/"
-                          className="navbar-brand-hover text-2xl navbar-brand">SystemManager</Link>
+                          className="navbar-brand-hover text-xl navbar-brand">SystemManager</Link>
                 </div>
                 <div className="block lg:hidden">
                     <button
-                        className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                        <svg className="fill-current h-3 w-3" viewBox="0 0 20 20"
+                        className="flex items-center text-white hover:text-white hover:border-white">
+                        <svg className="fill-current h-6 w-7" viewBox="0 0 20 20"
                              xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
                             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
                         </svg>
                     </button>
                 </div>
-                <div className="">
+                <div className="w-full lg:flex lg:items-center lg:w-auto hidden sm:hidden">
                     <Link title="Home page"
                           className="block mt-4 lg:inline-block lg:mt-0 text-white primary-light-text mr-4"
                           to="/">Home</Link>
