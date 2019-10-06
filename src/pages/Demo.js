d7cb7b78ce51eb5723392b5f6377f9dae3b17b4a
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-10-06 19:14:55 +0200

    Trying a new fontface

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import {Fragment} from "react";
 import SystemManagerShowcaseTopBar from "../components/TopBar";
+import * as Icons from "@fortawesome/react-fontawesome";
+import {faInfoCircle} from "@fortawesome/free-solid-svg-icons";
 
 
 export default class Demo extends React.Component {
@@ -9,16 +11,16 @@ export default class Demo extends React.Component {
             <Fragment>
                 <SystemManagerShowcaseTopBar/>
                 <div className="text-center showcase-padding">
-                    <h2>Not convinced yet? Use our demo to see what it can!</h2>
-                    <h5 hidden>This demo has certain features disabled and stuff hidden</h5>
-                    <h5>The SystemManager demo will be reset per 10 minutes</h5>
+                    <h2 className="text-2xl">Not convinced yet? Use our demo to see what it can!</h2>
+                    <h5><Icons.FontAwesomeIcon className="text-blue-700" icon={faInfoCircle}/> This demo has certain features disabled and stuff hidden</h5>
+                    <p className="text-info"><Icons.FontAwesomeIcon className="text-blue-700" icon={faInfoCircle}/> The SystemManager demo will be reset per 10 minutes</p>
                     <hr/>
                     <div className="flex flex-wrap">
                         <div className="w-full md:w-6/12 lg:w-4/12">
                             <div className="bg-white rounded overflow-hidden shadow-lg lg:m-4">
                                 <div className="px-6 py-4">
                                     <div className="text-black font-bold text-2xl mb-2">Demo Status Page</div>
-                                    <p className="text-gray-700 text-base p-4">
+                                    <p className="text-gray-700 text-base  p-4">
                                         <a href="https://status.demo.systemmanager.io"
                                            title="See the demo status page"
                                            className="btn systemManagerButton systemManagerButton-border">Look at the
