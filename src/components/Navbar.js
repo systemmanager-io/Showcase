519e7f947df126d459fe04f1238a402b22befc85
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-11-20 10:36:33 +0100

    Changed documentation to a seperate domain

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,16 +39,16 @@ export default class SystemManagerShowcaseNavbar extends React.Component {
                         <Link title="Demos of SystemManager"
                               className="block mt-4 lg:inline-block lg:mt-0 text-white primary-light-text mr-4"
                               to="/demo">Demo</Link>
-                        <Link title="Documentation of SystemManager"
+                        <a title="Documentation of SystemManager"
                               className="block mt-4 lg:inline-block lg:mt-0 text-white primary-light-text mr-4"
-                              to="/documentation">Documentation</Link>
+                              href="https://docs.systemmanager.io">Documentation</a>
                         <Link title="Plugins for SystemManager"
                               className="block mt-4 lg:inline-block lg:mt-0 text-white primary-light-text mr-4"
                               to="/plugins">Plugins <span className="text-xs text-pink-400">(Coming Soon)</span></Link>
 
-                        <Link title="Download SystemManager"
+                        <a title="Download SystemManager"
                               className="inline-block px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent primary-light-text hover:bg-white mt-4 lg:mt-0"
-                              to="/documentation/getstarted">Get Started</Link>
+                              href="https://docs.systemmanager.io/core/getting_started.html">Get Started</a>
                     </div>
                 </nav>
             </React.Fragment>
