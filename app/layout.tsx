81ae3a82440fe8c95b2de9b1a9c032cd7579a424
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2023-03-05 23:30:30 +0100

    Fix some files

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,14 @@ import SystemManagerShowcaseNavbar from "@/components/Navbar";
 import SystemManagerShowcaseTopBar from "@/components/TopBar";
 import Footer from "@/components/Footer";
 import FooterTigotech from "@/components/FooterTigotech";
+import React from "react";
 
 export const metadata = {
     title: 'SystemManager/FleetManager',
-    description: 'SystemManager. Server management tooling',
+    description: 'SystemManager. Server management tooling'
 }
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
-}) {
+export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <body>
